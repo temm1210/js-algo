2f7578a1aee19cb4ab34d130a24ecada8c43a90c
SongTaecWoo <temm1210@naver.com>
2021-05-03 14:40:08 +0900

    다익스트라

--- graph/dijkstra.js
+++ graph/dijkstra.js
@@ -29,3 +29,41 @@ function convertGraphToObject(graph) {
 
   return initObjectFromGraphNodeKeys;
 }
+
+// 다익스트라 알고리즘
+function dijkstra(graph, startNode) {
+  // 각각의 인접한 노드끼리 거리를 저장한 객체
+  const graphObject = convertGraphToObject(graph);
+
+  // 첫번째 거리는 0으로 초기화
+  graphObject[startNode] = 0;
+
+  // 시작점에서 다음노드까지 거리를 저장한 큐
+  const priorityQueue = factoryPriorityQueue(
+    [startNode, graphObject[startNode]],
+    (a, b) => a[1] > b[1]
+  );
+
+  while (!priorityQueue.isEmpty()) {
+    // 우선순위큐의 가장 작은값이 나옴
+    const [node, weight] = priorityQueue.pop();
+
+    Object.keys(graph[node]).map((adjacentNode) => {
+      // 인접노드 거리계산
+      const newDistance = graph[node][adjacentNode] + weight;
+
+      if (newDistance < graphObject[adjacentNode]) {
+        graphObject[adjacentNode] = newDistance;
+        priorityQueue.insert([adjacentNode, newDistance]);
+      }
+    });
+  }
+
+  console.log("graphObjec2t:", graphObject);
+}
+
+dijkstra(graph, "A");
+
+// 우선순위 큐에는 "시작지점"부터 특정노드까지의 총 거리가 저장되어있음
+// infinite값으로 초기화된 graph객체는 "시작지점" 부터가 아니라 각각 이전노드로 부터의 거리들이 저장되어있음 -> 이 값들을 업데이트 해나감
+// 우선순위 큐를 사용하면 불필요한 계산을 줄일 수 있음(우선 순위큐가 최소거리만 계속 계산하도록 도와줘서 최소거리보다 큰 값은 계산스킵이 가능함)
