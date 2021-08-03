72b69c968b70d8745ae0cae476fd5d7de94555a6
SongTaecWoo <temm1210@naver.com>
2021-08-03 22:09:26 +0900

    friend network(union find)

--- test2/findFriends.js
+++ test2/findFriends.js
@@ -39,27 +39,60 @@ function solution(arr) {
   console.log("visited:", visited.length);
 }
 
+const nodes = {};
+
+//
+// f1: {
+//   parent:f1,
+//   networkCount:1
+// }
 // union-find
 function solution2(friendArr) {
   const [f1, f2] = friendArr;
 
   if (!nodes[f1]) {
+    nodes[f1] = {};
     nodes[f1].parent = f1;
     nodes[f1].networkCount = 1;
   }
 
   if (!nodes[f2]) {
+    nodes[f2] = {};
     nodes[f2].parent = f2;
     nodes[f2].networkCount = 1;
   }
 
   union(f1, f2);
+
+  console.log("result:", nodes[f1].networkCount);
+}
+
+function union(f1, f2) {
+  const f1Parent = find(f1);
+  const f2Parent = find(f2);
+
+  if (f1Parent !== f2Parent) {
+    nodes[f2].parent = f1;
+    nodes[f1].networkCount += nodes[f2].networkCount;
+    nodes[f2].networkCount = nodes[f1].networkCount;
+  }
+}
+
+function find(f) {
+  if (f === nodes[f].parent) {
+    return f;
+  }
+  // path compression
+  const parent = find(nodes[f].parent);
+  nodes[f].parent = parent;
+  return nodes[f].parent;
 }
 
 rl.on("line", function (friends) {
   /**********************************/
   const friendArr = friends.split(" ");
-  solution(friendArr);
+  // solution(friendArr);
+  solution2(friendArr);
 
   /**********************************/
 }).on("close", function () {
